5ed99ef0d6860081461ee6eaecf3f1d752319957
qizongliang <qizongliang@gmail.com>
2024-02-16 11:32:49 -0500

    test8 enviorment

---
+++ test8.py
@@ -0,0 +1,8 @@
+# Load questions from the file animals.txt using the moduel readquiz.py
+# you will need atleast three global variables: the list of questions, the
+# number of times the player answered, and how many times they were correct:
+
+
+
+# create a Tkinter interface, arranging widgets as close as possible to the following layout
+# (you may use additonal Frame widgets to help arrange buttons and labels):
